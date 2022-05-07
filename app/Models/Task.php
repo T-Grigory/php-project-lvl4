8c0e661e65f1c007f892ffbd071a8baf03cc9c52
Grigory Tydykov <tydykov2022@mail.ru>
2022-05-07 15:39:55 +0700

    fix

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,33 +5,31 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Task extends Model
 {
     use HasFactory;
 
-    /**
-     * @var int
-     */
-    public mixed $created_by_id;
     protected $fillable = ['name', 'description', 'status_id', 'assigned_to_id', 'created_by_id'];
 
-    public function createdBy()
+    public function createdBy(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function assignedTo()
+    public function assignedTo(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function status()
+    public function status(): BelongsTo
     {
         return $this->belongsTo(TaskStatus::class);
     }
 
-    public function labels()
+    public function labels(): BelongsToMany
     {
         return $this->belongsToMany(Label::class);
     }
